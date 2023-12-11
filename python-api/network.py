87120b604f87df6aaefdbcb40f0da83d98cb3d73
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-12-11 09:10:49 -0800

    Refactor network.py

--- python-api/network.py
+++ python-api/network.py
@@ -1,6 +1,7 @@
 from mininet.net import Mininet
 from mininet.node import OVSKernelSwitch
-from mininet.topo import LinearTopo, Topo
+from mininet.log import setLogLevel, info
+from mininet.topo import Topo
 from mininet.node import RemoteController, CPULimitedHost
 from mininet.link import TCLink
 import random
@@ -9,124 +10,107 @@ import random
 global net
 net:Mininet = None
 
-class CustomTopo(Topo):
+class NsfnetTopo(Topo):
     def build( self, **params ):
-        #hosts = [ self.addHost('h%s' % h) for h in range(1,numberOfHostes + 1)]
-        #switches = [ self.addSwitch('s%s' % s) for s in range(1,numberOfSwitches + 1)]
+        """
+        Topology Link: https://github.com/BNN-UPC/NetworkModelingDatasets/blob/master/assets/nsfnet_topology.png
+        """
+        # Hosts
+        h0 = self.addHost('h0', ip='10.0.0.1')
+        h1 = self.addHost('h1', ip='10.0.0.2')
+        h2 = self.addHost('h2', ip='10.0.0.3')
+        h3 = self.addHost('h3', ip='10.0.0.4')
+        h4 = self.addHost('h4', ip='10.0.0.5')
+        h5 = self.addHost('h5', ip='10.0.0.6')
+        h6 = self.addHost('h6', ip='10.0.0.7')
+        h7 = self.addHost('h7', ip='10.0.0.8')
+        h8 = self.addHost('h8', ip='10.0.0.9')
+        h9 = self.addHost('h9', ip='10.0.0.10')
+        h10 = self.addHost('h10', ip='10.0.0.11')
+        h11 = self.addHost('h11', ip='10.0.0.12')
+        h12 = self.addHost('h12', ip='10.0.0.13')
+        h13 = self.addHost('h13', ip='10.0.0.14')
 
-        #hosts
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
-        h5 = self.addHost('h5')
-        h6 = self.addHost('h6')
-        h7 = self.addHost('h7')
-        h8 = self.addHost('h8')
-        h9 = self.addHost('h9')
-        h10 = self.addHost('h10')
-        h11 = self.addHost('h11')
-        h12 = self.addHost('h12')
-        h13 = self.addHost('h13')
-        h14 = self.addHost('h14')
-        h15 = self.addHost('h15')
-        h16 = self.addHost('h16')
-        h17 = self.addHost('h17')
+        # Switches
+        s0 = self.addSwitch('s0', dpid='00:00:00:00:00:00:00:01', protocols="OpenFlow13")
+        s1 = self.addSwitch('s1', dpid='00:00:00:00:00:00:00:02', protocols="OpenFlow13")
+        s2 = self.addSwitch('s2', dpid='00:00:00:00:00:00:00:03', protocols="OpenFlow13")
+        s3 = self.addSwitch('s3', dpid='00:00:00:00:00:00:00:04', protocols="OpenFlow13")
+        s4 = self.addSwitch('s4', dpid='00:00:00:00:00:00:00:05', protocols="OpenFlow13")
+        s5 = self.addSwitch('s5', dpid='00:00:00:00:00:00:00:06', protocols="OpenFlow13")
+        s6 = self.addSwitch('s6', dpid='00:00:00:00:00:00:00:07', protocols="OpenFlow13")
+        s7 = self.addSwitch('s7', dpid='00:00:00:00:00:00:00:08', protocols="OpenFlow13")
+        s8 = self.addSwitch('s8', dpid='00:00:00:00:00:00:00:09', protocols="OpenFlow13")
+        s9 = self.addSwitch('s9', dpid='00:00:00:00:00:00:00:10', protocols="OpenFlow13")
+        s10 = self.addSwitch('s10', dpid='00:00:00:00:00:00:00:11', protocols="OpenFlow13")
+        s11 = self.addSwitch('s11', dpid='00:00:00:00:00:00:00:12', protocols="OpenFlow13")
+        s12 = self.addSwitch('s12', dpid='00:00:00:00:00:00:00:13', protocols="OpenFlow13")
+        s13 = self.addSwitch('s13', dpid='00:00:00:00:00:00:00:14', protocols="OpenFlow13")
 
-        #switches
-        s1 = self.addSwitch('s1')
-        s2 = self.addSwitch('s2')
-        s3 = self.addSwitch('s3')
-        s4 = self.addSwitch('s4')
-        s5 = self.addSwitch('s5')
-        s6 = self.addSwitch('s6')
-        s7 = self.addSwitch('s7')
-        s8 = self.addSwitch('s8')
-        s9 = self.addSwitch('s9')
-        s10 = self.addSwitch('s10')
-        s11 = self.addSwitch('s11')
-        s12 = self.addSwitch('s12')
-        s13 = self.addSwitch('s13')
-        s14 = self.addSwitch('s14')
-        s15 = self.addSwitch('s15')
-        s16 = self.addSwitch('s16')
-        s17 = self.addSwitch('s17')
-        s18 = self.addSwitch('s18')
-        s19 = self.addSwitch('s19')
-        s20 = self.addSwitch('s20')
-        s21 = self.addSwitch('s21')
-        s22 = self.addSwitch('s22')
+        # Links
+        # Link options
+        linkopts1 = dict(delay='25ms', bw=100, loss=0, max_queue_size=1000, use_htb=True) # between switch and host
+        linkopts2 = dict(delay='25ms', bw=100, loss=0, max_queue_size=2000, use_htb=True) # between 2 switches
 
-        #Links
-        linkopts1 = dict(delay='1ms', bw=100, loss=1, max_queue_size=1000, use_htb=True) # between switch and host
-        linkopts2 = dict(bw=1000, delay='1ms', loss=1, use_htb=True) # between 2 switches
-
-        # swith to switch
-        # first path
+        # Switch to switch
+        self.addLink(s0, s1, **linkopts2)
+        self.addLink(s0, s2, **linkopts2)
+        self.addLink(s0, s3, **linkopts2)
+        self.addLink(s1, s7, **linkopts2)
         self.addLink(s1, s2, **linkopts2)
-        self.addLink(s2, s3, **linkopts2)
+        self.addLink(s2, s5, **linkopts2)
         self.addLink(s3, s4, **linkopts2)
+        self.addLink(s3, s8, **linkopts2)
         self.addLink(s4, s5, **linkopts2)
-        self.addLink(s5, s6, **linkopts2)
+        self.addLink(s4, s6, **linkopts2)
+        self.addLink(s5, s12, **linkopts2)
+        self.addLink(s5, s13, **linkopts2)
         self.addLink(s6, s7, **linkopts2)
-        self.addLink(s7, s8, **linkopts2)
-
-        # second path
-        self.addLink(s1, s9, **linkopts2)
+        self.addLink(s7, s10, **linkopts2)
+        self.addLink(s8, s9, **linkopts2)
+        self.addLink(s8, s11, **linkopts2)
         self.addLink(s9, s10, **linkopts2)
+        self.addLink(s9, s12, **linkopts2)
         self.addLink(s10, s11, **linkopts2)
+        self.addLink(s10, s13, **linkopts2)
         self.addLink(s11, s12, **linkopts2)
-        self.addLink(s12, s13, **linkopts2)
-        self.addLink(s13, s14, **linkopts2)
-        self.addLink(s14, s15, **linkopts2)
-        self.addLink(s15, s16, **linkopts2)
-        self.addLink(s16, s17, **linkopts2)
-        self.addLink(s17, s18, **linkopts2)
-        self.addLink(s18, s19, **linkopts2)
-        self.addLink(s19, s20, **linkopts2)
-        self.addLink(s20, s21, **linkopts2)  
-        self.addLink(s21, s22, **linkopts2)
 
-        self.addLink(s2, s9, **linkopts2)
-
-        # swith to host / First Path
+        # swith to host
+        self.addLink(s0, h0, **linkopts1)
         self.addLink(s1, h1, **linkopts1)
-        self.addLink(s3, h2, **linkopts1)
+        self.addLink(s2, h2, **linkopts1)
         self.addLink(s3, h3, **linkopts1)
-        self.addLink(s5, h4, **linkopts1)
+        self.addLink(s4, h4, **linkopts1)
         self.addLink(s5, h5, **linkopts1)
-        self.addLink(s7, h6, **linkopts1)
+        self.addLink(s6, h6, **linkopts1)
         self.addLink(s7, h7, **linkopts1)
         self.addLink(s8, h8, **linkopts1)
-        self.addLink(s8, h9, **linkopts1)
-
-        # swith to host / Second Path
-        self.addLink(s17, h10, **linkopts1)
-        self.addLink(s17, h11, **linkopts1)
-        self.addLink(s19, h12, **linkopts1)
-        self.addLink(s19, h13, **linkopts1)
-        self.addLink(s21, h14, **linkopts1)
-        self.addLink(s21, h15, **linkopts1)
-        self.addLink(s22, h16, **linkopts1)
-        self.addLink(s22, h17, **linkopts1)
+        self.addLink(s9, h9, **linkopts1)
+        self.addLink(s10, h10, **linkopts1)
+        self.addLink(s11, h11, **linkopts1)
+        self.addLink(s12, h12, **linkopts1)
+        self.addLink(s13, h13, **linkopts1)
 
 def buildCustomTopo():
-    topo = CustomTopo()
+    topo = NsfnetTopo()
     return topo
 
 def initalizeTopology(topo):
     global net
-    net = Mininet(topo=topo, build=False, switch=OVSKernelSwitch, link=TCLink, autoStaticArp=True, cleanup=True, autoSetMacs=True)
+    net = Mininet(topo=topo, link=TCLink, build=False, switch=OVSKernelSwitch, autoSetMacs=True, waitConnected=True)
     
     # Adding floodlight controller here
-    remoteControllerIp = "127.0.0.1"
-    net.addController("c1", controller=RemoteController, ip=remoteControllerIp, port=6653)
+    remote_ip = "127.0.0.1"
+    info('** Adding Floodlight Controller\n')
+    net.addController('c1', controller=RemoteController, ip=remote_ip, port=6653, protocols="OpenFlow13")
+
+    # Build the network
     net.build()
     net.start()
 
 def pingAllTest():
-    global nets
-    print("PING ALL RESULT") 
+    global net
+    info("PING ALL RESULT") 
     net.pingAll()
 
 def testTopology():
@@ -166,7 +150,6 @@ def getHosts():
 def getLinks():
     global net
     return net.links
-
 
 def getSwitches():
     global net
