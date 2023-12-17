dbd32bd7ec313be800d9ea0c957b907cd2d16c8f
Dursun <93613110+Dursunipek@users.noreply.github.com>
2023-12-17 11:35:27 -0800

    Add network performance test codes

--- topologies/nsfnet_topology2.py
+++ topologies/nsfnet_topology2.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python
 
+from ast import List
 import time
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.net import Mininet
-from mininet.node import RemoteController, OVSKernelSwitch
+from mininet.node import RemoteController, OVSKernelSwitch, CPULimitedHost, Host
 from mininet.topo import Topo
 from mininet.link import TCLink
+import json
+import pingparsing
+import threading
+from threading import Thread
 
 class NsfnetTopo(Topo):
     """
@@ -14,20 +19,31 @@ class NsfnetTopo(Topo):
     """
     def build( self, **params ):
         # Hosts
-        h0 = self.addHost('h0', ip='10.0.0.1')
-        h1 = self.addHost('h1', ip='10.0.0.2')
-        h2 = self.addHost('h2', ip='10.0.0.3')
-        h3 = self.addHost('h3', ip='10.0.0.4')
-        h4 = self.addHost('h4', ip='10.0.0.5')
-        h5 = self.addHost('h5', ip='10.0.0.6')
-        h6 = self.addHost('h6', ip='10.0.0.7')
-        h7 = self.addHost('h7', ip='10.0.0.8')
-        h8 = self.addHost('h8', ip='10.0.0.9')
-        h9 = self.addHost('h9', ip='10.0.0.10')
-        h10 = self.addHost('h10', ip='10.0.0.11')
-        h11 = self.addHost('h11', ip='10.0.0.12')
-        h12 = self.addHost('h12', ip='10.0.0.13')
-        h13 = self.addHost('h13', ip='10.0.0.14')
+        hostCount = 12
+        h0 = self.addHost('h0', ip='10.0.0.1', cpu=0.8/hostCount)
+        # h1 = self.addHost('h1', ip='10.0.0.2', cpu=0.8/hostCount)
+        # h2 = self.addHost('h2', ip='10.0.0.3', cpu=0.8/hostCount)
+        h3 = self.addHost('h3', ip='10.0.0.4', cpu=0.8/hostCount)
+        # h4 = self.addHost('h4', ip='10.0.0.5', cpu=0.8/hostCount)
+        # h5 = self.addHost('h5', ip='10.0.0.6', cpu=0.8/hostCount)
+        # h6 = self.addHost('h6', ip='10.0.0.7', cpu=0.8/hostCount)
+        # h7 = self.addHost('h7', ip='10.0.0.8', cpu=0.8/hostCount)
+        h8 = self.addHost('h8', ip='10.0.0.9', cpu=0.8/hostCount)
+        h9 = self.addHost('h9', ip='10.0.0.10', cpu=0.8/hostCount)
+        # h10 = self.addHost('h10', ip='10.0.0.11', cpu=0.8/hostCount)
+        # h11 = self.addHost('h11', ip='10.0.0.12', cpu=0.8/hostCount)
+        # h12 = self.addHost('h12', ip='10.0.0.13', cpu=0.8/hostCount)
+        # h13 = self.addHost('h13', ip='10.0.0.14', cpu=0.8/hostCount)
+        h14 = self.addHost('h14', ip='10.0.0.15', cpu=0.8/hostCount)
+        h15 = self.addHost('h15', ip='10.0.0.16', cpu=0.8/hostCount)
+        h16 = self.addHost('h16', ip='10.0.0.17', cpu=0.8/hostCount)
+        h17 = self.addHost('h17', ip='10.0.0.18', cpu=0.8/hostCount)
+        # h18 = self.addHost('h18', ip='10.0.0.19', cpu=0.8/hostCount)
+        # h19 = self.addHost('h19', ip='10.0.0.20', cpu=0.8/hostCount)
+        # h20 = self.addHost('h20', ip='10.0.0.21', cpu=0.8/hostCount)
+        # h21 = self.addHost('h21', ip='10.0.0.22', cpu=0.8/hostCount)
+
+
 
         # Switches
         s0 = self.addSwitch('s0', dpid='00:00:00:00:00:00:00:01', protocols="OpenFlow13")
@@ -47,8 +63,8 @@ class NsfnetTopo(Topo):
 
         # Links
         # Link options
-        linkopts1 = dict(delay='25ms', bw=100, loss=0, max_queue_size=1000, use_htb=True) # between switch and host
-        linkopts2 = dict(delay='25ms', bw=100, loss=0, max_queue_size=2000, use_htb=True) # between 2 switches
+        linkopts1 = dict(delay='25ms', bw=10, loss=0, max_queue_size=1000, use_htb=True) # between switch and host
+        linkopts2 = dict(delay='25ms', bw=10, loss=0, max_queue_size=1000, use_htb=True) # between 2 switches
 
         # Switch to switch
         self.addLink(s0, s1, **linkopts2)
@@ -75,19 +91,33 @@ class NsfnetTopo(Topo):
 
         # swith to host
         self.addLink(s0, h0, **linkopts1)
-        self.addLink(s1, h1, **linkopts1)
-        self.addLink(s2, h2, **linkopts1)
+        # self.addLink(s1, h1, **linkopts1)
+        # self.addLink(s2, h2, **linkopts1)
         self.addLink(s3, h3, **linkopts1)
-        self.addLink(s4, h4, **linkopts1)
-        self.addLink(s5, h5, **linkopts1)
-        self.addLink(s6, h6, **linkopts1)
-        self.addLink(s7, h7, **linkopts1)
+        # self.addLink(s4, h4, **linkopts1)
+        # self.addLink(s5, h5, **linkopts1)
+        # self.addLink(s6, h6, **linkopts1)
+        # self.addLink(s7, h7, **linkopts1)
         self.addLink(s8, h8, **linkopts1)
         self.addLink(s9, h9, **linkopts1)
-        self.addLink(s10, h10, **linkopts1)
-        self.addLink(s11, h11, **linkopts1)
-        self.addLink(s12, h12, **linkopts1)
-        self.addLink(s13, h13, **linkopts1)
+        # self.addLink(s10, h10, **linkopts1)
+        # self.addLink(s11, h11, **linkopts1)
+        # self.addLink(s12, h12, **linkopts1)
+        # self.addLink(s13, h13, **linkopts1)
+
+        #-------------------------------------
+        self.addLink(s3, h14, **linkopts1)
+        self.addLink(s3, h15, **linkopts1)
+        self.addLink(s8, h16, **linkopts1)
+        self.addLink(s8, h17, **linkopts1)
+        # self.addLink(s3, h18, **linkopts1)
+        # self.addLink(s3, h19, **linkopts1)
+        # self.addLink(s8, h20, **linkopts1)
+        # self.addLink(s8, h21, **linkopts1)
+        
+
+        
+
         
 def startNetwork():
     global net
@@ -95,14 +125,65 @@ def startNetwork():
 
     remote_ip = "127.0.0.1"
     info('** Adding Floodlight Controller\n')
-    net.addController('c1', controller=RemoteController, ip=remote_ip, port=6653, protocols="OpenFlow13")
+    net.addController('c1', controller=RemoteController, host=CPULimitedHost, ip=remote_ip, port=6653, protocols="OpenFlow13")
 
     # Build the network
     net.build()
     net.start()
+    time.sleep(2)
     net.pingAll()
+    getPingStats("h0", "h9")
+    getIperfTcpStats("h0", "h9")
     CLI(net)
     net.stop()
+
+def getPingStats(sender: str, receiver: str) -> [float, float]:
+    senderNode, receiverNode = net.getNodeByName(sender), net.getNodeByName(receiver)
+    receiverIpv4 = receiverNode.IP()
+    print("Receiver ipv4 -> ", receiverIpv4)
+    command = f"ping {receiverIpv4} -c 1000 -f"
+    print(command)
+    result = senderNode.cmd(command)
+
+    ping_parser = pingparsing.PingParsing()
+    # print(json.dumps(ping_parser.parse(result).as_dict(), indent=4))
+    result = ping_parser.parse(result).as_dict()
+    packetLoss = result["packet_loss_rate"]
+    avgRTT = result["rtt_avg"]
+    print(f"""Average RTT: {avgRTT}\nPacket Loss: {packetLoss}""")
+    return avgRTT, packetLoss
+
+def getIperfTcpStats(server: str, client: str):
+    server, client = net.getNodeByName(server), net.getNodeByName(client)
+    serverCommand = "iperf3 -s -p 5555 -i 1 -1"
+    clientCommand = f"iperf3 -c {server.IP()} -p 5555 -b 4M -R -t 5 -J"
+    # thread1 = threading.Thread(target=hostCommand, args=(server, serverCommand,))
+    # thread2 = threading.Thread(target=hostCommand, args=(client, clientCommand,))
+    serverThread = HostCommand(server, serverCommand)
+    clientThread = HostCommand(client, clientCommand) 
+
+    serverThread.start()
+    time.sleep(1)
+    clientThread.start()
+
+    serverThread.join()
+    clientThread.join()
+
+    print(json.loads(clientThread.result)["end"])
+
+# def hostCommand(host: Host, command: str):
+#     result = host.cmd(command)
+#     return result
+ 
+# custom thread
+class HostCommand(Thread):
+    def __init__(self, host: Host, command: str):
+        Thread.__init__(self)
+        self._host = host
+        self._command = command
+        self.result = None
+    def run(self):
+        self.result = self._host.cmd(self._command)
 
 if __name__ == '__main__':
     setLogLevel('info')
